9b193f23cbc13cefcad74ff404bc3b518c27db0b
Andrew C. Young <andrew@vaelen.org>
2025-01-16 17:10:24 +0900

    Cleaned up command handling, adding memory printing

--- src/main.c
+++ src/main.c
@@ -67,6 +67,39 @@ void print_pc(address value) {
   printf("PC : %04X\n", value);
 }
 
+void print_memory_header() {
+  puts("       0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F");
+}
+
+void print_memory_location(address a) {
+  printf("%04X: ", a);
+}
+
+void print_memory(cpu *c, address start, address end) {
+  address current = start;
+  int column = 0, row = 0;
+  byte b = 0;
+
+  while (current <= end) {
+    if (column == 0) {
+      if (current != start) {
+	/* new line */
+	puts("");
+      }
+      if (row == 0) {
+	print_memory_header();
+      }
+      row = (row + 1) % 23;
+      print_memory_location(current);
+    }
+    printf("%02X ", c->read(current));
+
+    current = current + 1;
+    column = (column + 1) % 16;
+  }
+  puts("");
+}
+
 void print_help() {
   puts("Commands:");
   puts("  H | HELP  - show this help screen");
@@ -84,23 +117,70 @@ void print_help() {
   puts("  SR [FF]   - print or set the status register");
   puts("  SP [FF]   - print or set the stack pointer");
   puts("");
-  puts("Simple Memory Access:");
-  puts("  M FFFF          - show value at address FFFF");
-  puts("  M FF00.FFFF     - show values from address FF00 to FFFF");
-  puts("  M FFFF FF[FE..] - set values starting at address FFFF");
-  puts("  M FF00.FFFF FF  - set values from address FF00 to FFFF");
+  puts("Memory Access:");
+  puts("  M                - print values of addresses 0000 to 00FF");
+  puts("  M FFFF           - print value at address FFFF");
+  puts("  M FF00.FFFF      - print values of addresses FF00 to FFFF");
+  puts("  M FFFF: FF[FE..] - set values starting at address FFFF");
+  puts("  M FF00.FFFF: FF  - set addresses FF00 to FFFF to the value FF");
   puts("");
+  puts("Data Import / Export:");
+  puts("  IMPORT SREC - Import Motorola S-Record formatted data.");
+  puts("  EXPORT SREC - Export Motorola S-Record formatted data.");
 }
 
 int starts_with(char *prefix, char *s) {
   return !strncmp(prefix, s, strlen(prefix));
 }
 
+void not_implemented() {
+  puts("Not Yet Implemented");
+}
+
+int is_whitespace(char c) {
+  switch (c) {
+  case '\n':
+  case '\r':
+  case '\t':
+  case '\f':
+  case '\v':
+  case ' ':
+    return 1;
+  }
+  return 0;
+}
+
+/** Removes leading whitespace **/
+char *ltrim(char *s) {
+  char *trimmed = s;
+  while (trimmed[0] != 0) {
+    if (is_whitespace(trimmed[0])) {
+      trimmed = trimmed + 1;
+    } else {
+      return trimmed;
+    }
+  }
+  return trimmed;
+}
+
+/** Removes trailing whitespace **/
+void rtrim(char *s) {
+  int i = strlen(s);
+  while (i > 0) {
+    i = i - 1;
+    if (is_whitespace(s[i])) {
+      s[i] = 0;
+    } else {
+      return;
+    }
+  }
+}
+
 int main() {
 
   cpu c;
-  int l = 0, done = 0;
-  char cmd[256];
+  int l = 0, done = 0, cmdlen = 0;
+  char cmdbuf[256], *cmd;
   address a = 0;
   byte b = 0;
 
@@ -119,47 +199,80 @@ int main() {
   while (!done) {
     printf("=> ");
 
-    l = read_line(cmd, 255);
+    l = read_line(cmdbuf, 255);
     if (l >= 0) {
-      cmd[l] = 0;
+      cmdbuf[l] = 0;
     }
 
+    cmd = cmdbuf;
+
+    cmd = ltrim(cmd);
+    rtrim(cmd);
+
+    puts(cmd);
+    
+    cmdlen = strlen(cmd);
+    
     if (l == EOF) {
       done = 1;
-    } else if ((strlen(cmd) == 1 && starts_with("H", cmd)) ||
+    } else if (cmdlen == 0) {
+      /** Empty command, do nothing. */
+    } else if ((cmdlen == 1 && starts_with("H", cmd)) ||
 	       starts_with("HELP", cmd)) {
       print_help();
-    } else if ((strlen(cmd) == 1 && starts_with("Q", cmd)) ||
+    } else if ((cmdlen == 1 && starts_with("Q", cmd)) ||
 	       starts_with("QUIT", cmd)) {
       done = 1;
-    } else if ((strlen(cmd) == 1 && starts_with("R", cmd)) ||
+    } else if ((cmdlen == 1 && starts_with("R", cmd)) ||
 	       starts_with("RESET", cmd)) {
       cpu_reset(&c);
-    } else if ((strlen(cmd) == 1 && starts_with("S", cmd)) ||
+    } else if ((cmdlen == 1 && starts_with("S", cmd)) ||
 	       starts_with("STEP", cmd)) {
       cpu_step(&c);
-    } else if ((strlen(cmd) == 1 && starts_with("G", cmd)) ||
-	       starts_with("GO", cmd)) {
+    } else if ((cmdlen == 1 && starts_with("G", cmd)) ||
+	       (cmdlen == 2 && starts_with("GO", cmd))) {
       cpu_run(&c);
-    } else if (starts_with("?", cmd)) {
+    } else if (cmdlen == 1 && starts_with("?", cmd)) {
       print_pc(c.pc);
       print_register(" A", c.a);
       print_register(" X", c.x);
       print_register(" Y", c.y);
       print_register("SR", c.sr);
       print_register("SP", c.sp);
-    } else if (starts_with("PC", cmd)) {
+    } else if (cmdlen == 2 && starts_with("PC", cmd)) {
       print_pc(c.pc);
-    } else if (starts_with("A", cmd)) {
+    } else if (starts_with("PC ", cmd)) {
+      not_implemented();
+    } else if (cmdlen == 1 && starts_with("A", cmd)) {
       print_register("A", c.a);
-    } else if (starts_with("X", cmd)) {
+    } else if (starts_with("PC ", cmd)) {
+      not_implemented();
+    } else if (cmdlen == 1 && starts_with("X", cmd)) {
       print_register("X", c.x);
-    } else if (starts_with("Y", cmd)) {
+    } else if (starts_with("PC ", cmd)) {
+      not_implemented();
+    } else if (cmdlen == 1 && starts_with("Y", cmd)) {
       print_register("Y", c.y);
-    } else if (starts_with("SR", cmd)) {
+    } else if (starts_with("PC ", cmd)) {
+      not_implemented();
+    } else if (cmdlen == 2 && starts_with("SR", cmd)) {
       print_register("SR", c.sr);
-    } else if (starts_with("SP", cmd)) {
+    } else if (starts_with("PC ", cmd)) {
+      not_implemented();
+    } else if (cmdlen == 2 && starts_with("SP", cmd)) {
       print_register("SP", c.sp);
+    } else if (starts_with("PC ", cmd)) {
+      not_implemented();
+    } else if (cmdlen == 1 && starts_with("M", cmd)) {
+      print_memory(&c, 0x0000, 0x00FF);
+    } else if (starts_with("M ", cmd)) {
+      not_implemented();
+    } else if (starts_with("IMPORT ", cmd)) {
+      not_implemented();
+    } else if (starts_with("EXPORT ", cmd)) {
+      not_implemented();
+    } else {
+      printf("Invalid command: %s\n", cmd);
     }
     
   }
