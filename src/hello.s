ca88550c6b7845e62e119e424d4e7c4912c0e8b3
Andrew C. Young <andrew@vaelen.org>
2025-01-29 23:53:04 +0900

    Convert to c65 format.

--- src/hello.s
+++ src/hello.s
@@ -1,19 +1,19 @@
 	;;  A simple Hello World program.
 
-	ORG $1000
+	.ORG $1000
 
 	LDX #$FF
 	TXS
 
 	LDX #$0
-LOOP	LDA MSG,X
+LOOP:	LDA MSG,X
 	BEQ DONE
 	STA $FF00
 	INX
 	JMP LOOP
 
-DONE	LDA #$0A
+DONE:	LDA #$0A
 	STA $FF00
 	BRK
 	
-MSG	asciiz "Hello, world!"
+MSG:	.asciiz "Hello, world!"
